0052b2bbc62e3537cd2a10b8e10541f5cedad8af
조가린 <41724184+gareen9342@users.noreply.github.com>
2021-06-08 09:26:48 +0900

    yooseop setting;

--- README.md
+++
@@ -1,2 +0,0 @@
-# final-react-test-environment
-개발환경 세팅 기록
--- client.jsx
+++
@@ -1,7 +0,0 @@
-const React = require('react');
-const ReactDom = require('react-dom');
-
-const HelloWorld = require('./HelloWorld');
-
-ReactDom.render(<HelloWorld/>,document.querySelector('#root'));
-
